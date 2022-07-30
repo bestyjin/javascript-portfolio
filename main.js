8debc902a08a4c424f0e0df31e4ffff203d3ab92
yjkim <71006256+bestyjin@users.noreply.github.com>
2022-07-30 22:00:24 +0900

    JS : Navbar menu click

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 'use strict'
 
-// Make navbar transparent when it is on the top
+// 메뉴바가 멘 위에 위치할때 투명하게 변경
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', ()=>{
-    if(window.scrollY > navbarHeight){
-        console.log("a");
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
-        console.log("b");
         navbar.classList.remove('navbar--dark');
     }
+})
 
+// 메뉴 클릭시 해당 페이지로 스크롤 이동
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
 })
