3fea29d581cd88f2fd8d260fc44a655f155340e6
yjkim <71006256+bestyjin@users.noreply.github.com>
2022-07-30 22:08:41 +0900

    JS : Home contact button

--- main.js
+++ main.js
@@ -19,7 +19,17 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     }
-
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: "smooth" });
+    scrollIntoView(link);
 })
+
+// Home에 있는 'Contact me' 버튼 클릭 시 Contact 페이지로 스크롤 이동
+const HomeContactBtn = document.querySelector('.home__contact');
+HomeContactBtn.addEventListener('click', ()=>{
+    scrollIntoView('#contact')
+})
+
+// 스크롤 이동 함수
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+}
